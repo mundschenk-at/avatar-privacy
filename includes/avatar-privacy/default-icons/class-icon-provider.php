ebbacd44d327e44b87569c3f588751e0749838f7
Der Mundschenk & Compagnie <github@mundschenk.at>
2018-07-29 18:46:56 +0200

    Docs: fix incorrect class name

--- includes/avatar-privacy/default-icons/class-icon-provider.php
+++ includes/avatar-privacy/default-icons/class-icon-provider.php
@@ -36,7 +36,7 @@ namespace Avatar_Privacy\Default_Icons;
 interface Icon_Provider {
 
 	/**
-	 * Checks if this Default_Icon_Provider handles the given icon type.
+	 * Checks if this Icon_Provider handles the given icon type.
 	 *
 	 * @param  string $type The default icon type.
 	 *
