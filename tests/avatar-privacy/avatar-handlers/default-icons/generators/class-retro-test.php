f7b096ef88a0ee48ec184e923d8ebe532d562a58
Peter Putzer <github@mundschenk.at>
2019-04-13 20:03:11 +0200

    Tests: Add full coverage for Generators\Retro

---
+++ tests/avatar-privacy/avatar-handlers/default-icons/generators/class-retro-test.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * This file is part of Avatar Privacy.
+ *
+ * Copyright 2019 Peter Putzer.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *  ***
+ *
+ * @package mundschenk-at/avatar-privacy/tests
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace Avatar_Privacy\Tests\Avatar_Privacy\Avatar_Handlers\Default_Icons\Generators;
+
+use Brain\Monkey\Actions;
+use Brain\Monkey\Filters;
+use Brain\Monkey\Functions;
+
+use Mockery as m;
+
+use Avatar_Privacy\Avatar_Handlers\Default_Icons\Generators\Retro;
+
+/**
+ * Avatar_Privacy\Avatar_Handlers\Default_Icons\Generators\Retro unit test.
+ *
+ * @coversDefaultClass \Avatar_Privacy\Avatar_Handlers\Default_Icons\Generators\Retro
+ * @usesDefaultClass \Avatar_Privacy\Avatar_Handlers\Default_Icons\Generators\Retro
+ *
+ * @uses ::__construct
+ */
+class Retro_Test extends \Avatar_Privacy\Tests\TestCase {
+
+	/**
+	 * The system-under-test.
+	 *
+	 * @var Retro
+	 */
+	private $sut;
+
+	/**
+	 * The identicon mock.
+	 *
+	 * @var \Identicon\Identicon
+	 */
+	private $identicon;
+
+	/**
+	 * Alias mock of Colors\RandomColor.
+	 *
+	 * @var \Colors\RandomColor
+	 */
+	private $random_color;
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 */
+	protected function setUp() {
+		parent::setUp();
+
+		// Helper mocks.
+		$this->identicon    = m::mock( \Identicon\Identicon::class );
+		$this->random_color = m::mock( 'alias:' . \Colors\RandomColor::class );
+
+		// Partially mock system under test.
+		$this->sut = m::mock( Retro::class )->makePartial()->shouldAllowMockingProtectedMethods();
+
+		// Manually invoke the constructor as it is protected.
+		$this->invokeMethod( $this->sut, '__construct', [ $this->identicon ] );
+	}
+
+	/**
+	 * Tests ::__construct.
+	 *
+	 * @covers ::__construct
+	 */
+	public function test_constructor() {
+		$identicon = m::mock( \Identicon\Identicon::class );
+		$mock      = m::mock( Retro::class )->makePartial()->shouldAllowMockingProtectedMethods();
+
+		$this->invokeMethod( $mock, '__construct', [ $identicon ] );
+
+		$this->assertAttributeSame( $identicon, 'identicon', $mock );
+	}
+
+	/**
+	 * Tests ::build.
+	 *
+	 * @covers ::build
+	 */
+	public function test_build() {
+		$seed = 'fake email hash';
+		$size = 42;
+		$data = 'fake SVG image';
+
+		// Intermediate.
+		$bright_color = 'A bright color definition';
+		$light_color  = 'A light color definition';
+
+		$this->random_color->shouldReceive( 'one' )->once()->with( [ 'luminosity' => 'bright' ] )->andReturn( $bright_color );
+		$this->random_color->shouldReceive( 'one' )->once()->with( [ 'luminosity' => 'light' ] )->andReturn( $light_color );
+
+		$this->identicon->shouldReceive( 'getImageData' )->once()->with( $seed, $size, $bright_color, $light_color )->andReturn( $data );
+
+		$this->assertSame( $data, $this->sut->build( $seed, $size ) );
+	}
+}
