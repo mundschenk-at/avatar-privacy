df44300340efc3faa4bf6e9d62f6e9971af5a8b3
Peter Putzer <github@mundschenk.at>
2019-08-10 17:07:49 +0200

    Meta: Relax eslint rules for wpdiscuz file

--- public/js/wpdiscuz/use-gravatar.js
+++ public/js/wpdiscuz/use-gravatar.js
@@ -1,13 +1,18 @@
 /**
  * Resets the use_gravatar checkbox to the current value after posting a new comment.
+ *
  */
+
+/* global avatarPrivacy jQuery Cookies */
+/* eslint no-var: 0 */
+
 jQuery( function( $ ) {
 	'use strict';
 
 	var $useGravatarCheckbox = $( '#' + avatarPrivacy.checkbox );
 
 	var resetUseGravatar = function() {
-		var useGravatar = ( Cookies.get( avatarPrivacy.cookie ) != undefined && '' !== Cookies.get( avatarPrivacy.cookie ) ) ? 'checked' : '';
+		var useGravatar = ( Cookies.get( avatarPrivacy.cookie ) !== undefined && '' !== Cookies.get( avatarPrivacy.cookie ) ) ? 'checked' : '';
 
 		$useGravatarCheckbox.prop( 'checked', useGravatar );
 	};
