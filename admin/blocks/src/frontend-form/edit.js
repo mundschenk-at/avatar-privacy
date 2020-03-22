346f0d0e92b64baeeff7f4514752b695ee3b7455
Peter Putzer <github@mundschenk.at>
2020-03-22 16:53:54 +0100

    Whitespace (needed by latest WordPress JavaScript coding standards using prettier)

--- admin/blocks/src/frontend-form/edit.js
+++ admin/blocks/src/frontend-form/edit.js
@@ -9,7 +9,13 @@
 /**
  * WordPress dependencies
  */
-import { PanelBody, PanelRow, RangeControl, ServerSideRender, ToggleControl } from '@wordpress/components';
+import {
+	PanelBody,
+	PanelRow,
+	RangeControl,
+	ServerSideRender,
+	ToggleControl,
+} from '@wordpress/components';
 import { InspectorControls } from '@wordpress/editor';
 import { Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
@@ -18,22 +24,31 @@ export default ( { attributes, className, setAttributes } ) => {
 	return (
 		<Fragment>
 			<InspectorControls>
-				<PanelBody title={ __( 'Form', 'avatar-privacy' ) } >
+				<PanelBody title={ __( 'Form', 'avatar-privacy' ) }>
 					<PanelRow>
 						<RangeControl
 							label={ __( 'Avatar Size', 'avatar-privacy' ) }
 							value={ attributes.avatar_size }
 							initialPosition={ attributes.avatar_size }
-							onChange={ ( newSize ) => setAttributes( { avatar_size: newSize } ) }
+							onChange={ ( newSize ) =>
+								setAttributes( { avatar_size: newSize } )
+							}
 							min={ 48 }
 							max={ 240 }
 						/>
 					</PanelRow>
 					<PanelRow>
 						<ToggleControl
-							label={ __( 'Show Descriptions', 'avatar-privacy' ) }
+							label={ __(
+								'Show Descriptions',
+								'avatar-privacy'
+							) }
 							checked={ !! attributes.show_descriptions }
-							onChange={ () => setAttributes( { show_descriptions: ! attributes.show_descriptions } ) }
+							onChange={ () =>
+								setAttributes( {
+									show_descriptions: ! attributes.show_descriptions,
+								} )
+							}
 						/>
 					</PanelRow>
 				</PanelBody>
