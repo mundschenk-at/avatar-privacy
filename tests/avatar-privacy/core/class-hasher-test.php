31d237dec724f92ef8f26d4fbe20ca7b24f99a5a
Peter Putzer <github@mundschenk.at>
2020-05-31 13:30:32 +0200

    Extract Core::get_hash and ::get_salt to Hasher class

---
+++ tests/avatar-privacy/core/class-hasher-test.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * This file is part of Avatar Privacy.
+ *
+ * Copyright 2018-2020 Peter Putzer.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *  ***
+ *
+ * @package mundschenk-at/avatar-privacy/tests
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace Avatar_Privacy\Tests\Avatar_Privacy\Core;
+
+use Brain\Monkey\Actions;
+use Brain\Monkey\Filters;
+use Brain\Monkey\Functions;
+
+use Mockery as m;
+
+use Avatar_Privacy\Data_Storage\Network_Options;
+
+/**
+ * Avatar_Privacy_Factory unit test.
+ *
+ * @coversDefaultClass \Avatar_Privacy\Core\Hasher
+ * @usesDefaultClass \Avatar_Privacy\Core\Hasher
+ *
+ * @uses ::__construct
+ */
+class Hasher_Test extends \Avatar_Privacy\Tests\TestCase {
+
+	/**
+	 * The system-under-test.
+	 *
+	 * @var \Avatar_Privacy\Core\Hasher
+	 */
+	private $sut;
+
+	/**
+	 * Required helper object.
+	 *
+	 * @var Network_Options
+	 */
+	private $network_options;
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 *
+	 * @since 2.3.3 Renamed to `set_up`.
+	 */
+	protected function set_up() { // @codingStandardsIgnoreLine
+		parent::set_up();
+
+		// Mock required helpers.
+		$this->network_options = m::mock( Network_Options::class );
+
+		// Partially mock system under test.
+		$this->sut = m::mock(
+			\Avatar_Privacy\Core\Hasher::class,
+			[
+				$this->network_options,
+			]
+		)->makePartial()->shouldAllowMockingProtectedMethods();
+	}
+
+	/**
+	 * Test ::__construct.
+	 *
+	 * @covers ::__construct
+	 */
+	public function test_constructor() {
+		// Mock required helpers.
+		$network_options = m::mock( Network_Options::class )->makePartial();
+
+		$hasher = m::mock( \Avatar_Privacy\Core\Hasher::class )->makePartial();
+		$hasher->__construct( $network_options );
+
+		$this->assert_attribute_same( $network_options, 'network_options', $hasher );
+	}
+
+	/**
+	 * Tests ::get_salt.
+	 *
+	 * @covers ::get_salt
+	 */
+	public function test_get_salt() {
+		$expected_salt = 'random salt';
+
+		Filters\expectApplied( 'avatar_privacy_salt' )->once()->with( '' );
+
+		$this->network_options->shouldReceive( 'get' )->once()->with( Network_Options::SALT )->andReturn( '' );
+
+		Functions\expect( 'wp_rand' )->once()->andReturn( $expected_salt );
+
+		$this->network_options->shouldReceive( 'set' )->once()->with( Network_Options::SALT, $expected_salt );
+
+		$this->assertSame( $expected_salt, $this->sut->get_salt() );
+	}
+
+	/**
+	 * Tests ::get_salt.
+	 *
+	 * @covers ::get_salt
+	 */
+	public function test_get_salt_filtered() {
+		$expected_salt = 'random salt';
+
+		Filters\expectApplied( 'avatar_privacy_salt' )->once()->with( '' )->andReturn( $expected_salt );
+
+		$this->network_options->shouldReceive( 'get' )->never();
+		Functions\expect( 'wp_rand' )->never();
+		$this->network_options->shouldReceive( 'set' )->never();
+
+		$this->assertSame( $expected_salt, $this->sut->get_salt() );
+	}
+
+	/**
+	 * Tests ::get_salt.
+	 *
+	 * @covers ::get_salt
+	 */
+	public function test_get_salt_from_options() {
+		$expected_salt = 'random salt';
+
+		Filters\expectApplied( 'avatar_privacy_salt' )->once()->with( '' );
+
+		$this->network_options->shouldReceive( 'get' )->once()->with( Network_Options::SALT )->andReturn( $expected_salt );
+
+		Functions\expect( 'wp_rand' )->never();
+		$this->network_options->shouldReceive( 'set' )->never();
+
+		$this->assertSame( $expected_salt, $this->sut->get_salt() );
+	}
+
+	/**
+	 * Tests ::get_hash.
+	 *
+	 * @covers ::get_hash
+	 */
+	public function test_get_hash() {
+		$salt  = 'foobar57';
+		$email = 'example@example.org';
+
+		$this->sut->shouldReceive( 'get_salt' )->once()->andReturn( $salt );
+
+		$this->assertSame( '874ccc6634195fdf4a1e5391a623fddb8a347d26cad4d9bbda683923afca3132', $this->sut->get_hash( $email ) );
+	}
+
+	/**
+	 * Tests ::get_hash.
+	 *
+	 * @covers ::get_hash
+	 */
+	public function test_get_hash_with_whitespace() {
+		$salt  = 'foobar57';
+		$email = '   example@example.org ';
+
+		$this->sut->shouldReceive( 'get_salt' )->once()->andReturn( $salt );
+
+		$this->assertSame( '874ccc6634195fdf4a1e5391a623fddb8a347d26cad4d9bbda683923afca3132', $this->sut->get_hash( $email ) );
+	}
+}
