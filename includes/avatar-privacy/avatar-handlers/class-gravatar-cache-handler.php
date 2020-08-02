4190fe2ee1a978e0888fde98fb2f5b2619fb8f64
Peter Putzer <github@mundschenk.at>
2020-08-02 17:47:11 +0200

    Simplify Gravatar_Cache_Handler by not encoding 'user' vs. 'comment' into the file path

--- includes/avatar-privacy/avatar-handlers/class-gravatar-cache-handler.php
+++ includes/avatar-privacy/avatar-handlers/class-gravatar-cache-handler.php
@@ -41,37 +41,11 @@ use Avatar_Privacy\Tools\Network\Gravatar_Service;
  *
  * @since 1.0.0
  * @since 2.0.0 Class was moved to Avatar_Privacy\Avatar_Handlers and now implements the new Avatar_Handler interface.
+ * @since 2.4.0 Internal constants and property $type_mapping removed.
  *
  * @author Peter Putzer <github@mundschenk.at>
  */
 class Gravatar_Cache_Handler implements Avatar_Handler {
-
-	const TYPE_USER    = 'user';
-	const TYPE_COMMENT = 'comment';
-
-	const TYPE_MAPPING = [
-		'0' => self::TYPE_USER,
-		'1' => self::TYPE_COMMENT,
-		'2' => self::TYPE_USER,
-		'3' => self::TYPE_COMMENT,
-		'4' => self::TYPE_USER,
-		'5' => self::TYPE_COMMENT,
-		'6' => self::TYPE_USER,
-		'7' => self::TYPE_COMMENT,
-		'8' => self::TYPE_USER,
-		'9' => self::TYPE_COMMENT,
-		'a' => self::TYPE_USER,
-		'b' => self::TYPE_COMMENT,
-		'c' => self::TYPE_USER,
-		'd' => self::TYPE_COMMENT,
-		'e' => self::TYPE_USER,
-		'f' => self::TYPE_COMMENT,
-	];
-
-	const REVERSE_TYPE_MAPPING = [
-		true  => 'a', // TYPE_USER.
-		false => 'b', // TYPE_COMMENT.
-	];
 
 	/**
 	 * The core API.
@@ -95,13 +69,6 @@ class Gravatar_Cache_Handler implements Avatar_Handler {
 	private $file_cache;
 
 	/**
-	 * A copy of Gravatar_Cache_Handler::TYPE_MAPPING.
-	 *
-	 * @var string[]
-	 */
-	private $type_mapping;
-
-	/**
 	 * The Gravatar network service.
 	 *
 	 * @var Gravatar_Service
@@ -123,9 +90,6 @@ class Gravatar_Cache_Handler implements Avatar_Handler {
 		$this->options    = $options;
 		$this->file_cache = $file_cache;
 		$this->gravatar   = $gravatar;
-
-		// Needed for PHP 5.6 compatiblity.
-		$this->type_mapping = self::TYPE_MAPPING;
 	}
 
 	/**
@@ -155,8 +119,7 @@ class Gravatar_Cache_Handler implements Avatar_Handler {
 		];
 
 		$args     = \wp_parse_args( $args, $defaults );
-		$subdir   = $this->get_sub_dir( $hash, false !== $args['user_id'] );
-		$filename = "gravatar/{$subdir}/{$hash}-{$size}." . Images\Type::FILE_EXTENSION[ $args['mimetype'] ];
+		$filename = "gravatar/{$this->get_sub_dir( $hash )}/{$hash}-{$size}." . Images\Type::FILE_EXTENSION[ $args['mimetype'] ];
 
 		// Only retrieve new Gravatar if necessary.
 		if ( $args['force'] || ! \file_exists( "{$this->file_cache->get_base_dir()}{$filename}" ) ) {
@@ -177,23 +140,14 @@ class Gravatar_Cache_Handler implements Avatar_Handler {
 	 * Calculates the subdirectory from the given identity hash.
 	 *
 	 * @since 2.1.0 Visibility changed to protected.
+	 * @since 2.4.0 Parameter $user removed.
 	 *
 	 * @param  string $identity The identity (mail address) hash.
-	 * @param  bool   $user     If we need to encode the type as "user".
 	 *
 	 * @return string
 	 */
-	protected function get_sub_dir( $identity, $user = false ) {
-		$first  = \substr( $identity, 0, 1 );
-		$second = \substr( $identity, 1, 1 );
-
-		if ( ( $user && self::TYPE_USER === self::TYPE_MAPPING[ $first ] ) || ( ! $user && self::TYPE_COMMENT === self::TYPE_MAPPING[ $first ] ) ) {
-			$levels = [ $first, $second ];
-		} else {
-			$levels = [ self::REVERSE_TYPE_MAPPING[ $user ], $second ];
-		}
-
-		return \implode( '/', $levels );
+	protected function get_sub_dir( $identity ) {
+		return \implode( '/', \str_split( \substr( $identity, 0, 2 ) ) );
 	}
 
 	/**
@@ -210,20 +164,14 @@ class Gravatar_Cache_Handler implements Avatar_Handler {
 	 * @return bool              Returns `true` if successful, `false` otherwise.
 	 */
 	public function cache_image( $type, $hash, $size, $subdir, $extension ) {
-		// Determine hash type.
-		$type = \explode( '/', $subdir )[0];
-		if ( empty( $type ) || ! isset( $this->type_mapping[ $type ] ) ) {
-			return false;
-		}
 
 		// Lookup user and/or email address.
 		$user_id = false;
-		if ( self::TYPE_USER === $this->type_mapping[ $type ] ) {
-			$user = $this->core->get_user_by_hash( $hash );
-			if ( ! empty( $user ) ) {
-				$user_id = $user->ID;
-				$email   = ! empty( $user->user_email ) ? $user->user_email : '';
-			}
+		$user    = $this->core->get_user_by_hash( $hash );
+
+		if ( ! empty( $user ) ) {
+			$user_id = $user->ID;
+			$email   = ! empty( $user->user_email ) ? $user->user_email : '';
 		} else {
 			$email = $this->core->get_comment_author_email( $hash );
 		}
