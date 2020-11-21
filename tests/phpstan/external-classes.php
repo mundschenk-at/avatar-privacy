4286e91c205430fbc60ffd40231c0712d392921a
mundschenk_at <github@mundschenk.at>
2020-11-21 23:33:06 +0100

    Meta: Add PHPStan configuration

---
+++ tests/phpstan/external-classes.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This file is part of Avatar Privacy.
+ *
+ * Copyright 2020 Peter Putzer.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *  ***
+ *
+ * @package mundschenk-at/avatar-privacy/tests
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+// phpcs:disable WordPress.NamingConventions, Squiz.Commenting.ClassComment.Missing
+
+// WP User Manager stubs.
+
+namespace Carbon_Fields\Field {
+	class Field {
+		/**
+		 * Stub for WP User Manager.
+		 *
+		 * @return string
+		 */
+		public function get_base_name() {
+			return '';
+		}
+	}
+}
