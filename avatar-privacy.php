43f23115ec7a3ee6568b0adf9b05c58c6c37175b
mundschenk_at <github@mundschenk.at>
2021-01-05 22:18:36 +0100

    Bump version to 2.4.0-alpha.2

--- avatar-privacy.php
+++ avatar-privacy.php
@@ -29,7 +29,7 @@
  * Description: Adds options to enhance the privacy when using avatars.
  * Author: Peter Putzer
  * Author URI: https://code.mundschenk.at
- * Version: 2.4.0-alpha.1
+ * Version: 2.4.0-alpha.2
  * Requires at least: 5.2
  * Requires PHP: 7.0
  * License: GNU General Public License v2 or later
