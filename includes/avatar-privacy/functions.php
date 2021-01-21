bd38ea9d22ebd2ba8cf6436ef83cc7726b675486
Viktor Szépe <viktor@szepe.net>
2021-01-21 02:48:58 +0100

    Turn big comment block to file comment
    From https://github.com/squizlabs/PHP_CodeSniffer/blob/master/src/Standards/PEAR/Sniffs/Commenting/FileCommentSniff.php
    also see https://github.com/szepeviktor/small-project/blob/master/src/Config.php#L3-L11

--- includes/avatar-privacy/functions.php
+++ includes/avatar-privacy/functions.php
@@ -1,27 +1,13 @@
 <?php
+
 /**
- * This file is part of Avatar Privacy.
- *
- * Copyright 2018-2020 Peter Putzer.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- *  ***
+ * Procedural part of Avatar Privacy.
  *
  * @package mundschenk-at/avatar-privacy
- * @license http://www.gnu.org/licenses/gpl-2.0.html
+ * @author Peter Putzer <github@mundschenk.at>
+ * @copyright 2018-2020 Peter Putzer
+ * @license GPL-2.0-or-later http://www.gnu.org/licenses/gpl-2.0.txt
+ * @link https://code.mundschenk.at/avatar-privacy/
  */
 
 namespace Avatar_Privacy;
