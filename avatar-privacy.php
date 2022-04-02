ae160c0e0475d188f7ae90c24cb98e200e2705cf
mundschenk_at <github@mundschenk.at>
2022-04-02 17:18:25 +0200

    Require WordPress 5.6+

--- avatar-privacy.php
+++ avatar-privacy.php
@@ -30,7 +30,7 @@
  * Author: Peter Putzer
  * Author URI: https://code.mundschenk.at
  * Version: 2.6.0-alpha.1
- * Requires at least: 5.2
+ * Requires at least: 5.6
  * Requires PHP: 7.2
  * License: GNU General Public License v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
