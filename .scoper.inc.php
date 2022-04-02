bfef9206b2e30a43c808d9fee1adcda5d441d79f
mundschenk_at <github@mundschenk.at>
2022-04-03 01:17:32 +0200

    Meta: Update PHP-Scoper and WordPress symbol handling

--- .scoper.inc.php
+++ .scoper.inc.php
@@ -16,69 +16,6 @@ const AVATAR_PRIVACY_EXCLUDED_DIRS       = [
     // Partial templates will be copied by Grunt.
     'partials'
 ];
-// Global WordPress functions we use.
-const AVATAR_PRIVACY_WORDPRESS_FUNCTIONS = [
-    // Transients.
-    'get_transient',
-    'set_transient',
-    'delete_transient',
-    'get_site_transient',
-    'set_site_transient',
-    'delete_site_transient',
-
-    // Options.
-    'get_option',
-    'update_option',
-    'delete_option',
-    'get_network_option',
-    'update_network_option',
-    'delete_network_option',
-
-    // Object caching.
-    'wp_cache_get',
-    'wp_cache_set',
-    'wp_cache_delete',
-    'wp_using_ext_object_cache',
-
-    // Multisite.
-    'get_current_network_id',
-    'get_bloginfo',
-
-    // Escaping and sanitization.
-    'esc_attr',
-    'esc_html',
-    'esc_textarea',
-    'sanitize_text_field',
-    'wp_kses',
-    'wp_kses_allowed_html',
-
-    // Translation,
-    '__',
-    '_e',
-    'load_plugin_textdomain',
-
-    // Settings API.
-    'add_settings_field',
-
-    // Markup.
-    'checked',
-    'selected',
-
-    // Hooks.
-    'add_action',
-    'add_filter',
-    'remove_action',
-    'remove_filter',
-
-    // Plugins.
-    'plugin_basename',
-    'deactivate_plugins',
-
-    // Utility functions.
-    'wp_list_pluck',
-    'wp_parse_args',
-    'is_admin',
-];
 
 return [
     // The prefix configuration. If a non null value will be used, a random prefix will be generated.
@@ -142,7 +79,15 @@ return [
     // Whitelists a list of files. Unlike the other whitelist related features, this one is about completely leaving
     // a file untouched.
     // Paths are relative to the configuration file unless if they are already absolute
-    'files-whitelist' => [],
+    'exclude-files' => [],
+
+    'exclude-namespaces' => [],
+    'exclude-classes' => \array_merge(
+        \json_decode( \file_get_contents( 'vendor/sniccowp/php-scoper-wordpress-excludes/generated/exclude-wordpress-classes.json' ) ),
+        \json_decode( \file_get_contents( 'vendor/sniccowp/php-scoper-wordpress-excludes/generated/exclude-wordpress-interfaces.json' ) ),
+    ),
+    'exclude-functions' => \json_decode( \file_get_contents( 'vendor/sniccowp/php-scoper-wordpress-excludes/generated/exclude-wordpress-functions.json' ) ),
+    'exclude-constants' => \json_decode( \file_get_contents( 'vendor/sniccowp/php-scoper-wordpress-excludes/generated/exclude-wordpress-constants.json' ) ),
 
     // When scoping PHP files, there will be scenarios where some of the code being scoped indirectly references the
     // original namespace. These will include, for example, strings or string manipulations. PHP-Scoper has limited
@@ -150,54 +95,26 @@ return [
     // heart contents.
     //
     // For more see: https://github.com/humbug/php-scoper#patchers
+    /*
     'patchers' => [
         function (string $file_path, string $prefix, string $contents): string {
-            // Quote prefix.
-            $prefix = \preg_quote( $prefix, '#' );
-
-            if (
-                // Skip partials and dummy files.
-                \preg_match( '#.*/partials/\w+.*\.php$#', $file_path )
-            ) {
-                $contents = \preg_replace( "#\b{$prefix}\\\\([\w_]+\()#", '$1', $contents );
-            } elseif (
-                // Only for other PHP files.
-                \preg_match( '#\w+/\w+.*\.php#', $file_path )
-            ) {
-                // Un-preix WordPress functions.
-                $functions = \join( '|', AVATAR_PRIVACY_WORDPRESS_FUNCTIONS );
-                $contents  = \preg_replace( "/\b{$prefix}\\\\({$functions}\()/", '$1', $contents );
-            }
-
             return $contents;
         },
     ],
-
-    // PHP-Scoper's goal is to make sure that all code for a project lies in a distinct PHP namespace. However, you
-    // may want to share a common API between the bundled code of your PHAR and the consumer code. For example if
-    // you have a PHPUnit PHAR with isolated code, you still want the PHAR to be able to understand the
-    // PHPUnit\Framework\TestCase class.
-    //
-    // A way to achieve this is by specifying a list of classes to not prefix with the following configuration key. Note
-    // that this does not work with functions or constants neither with classes belonging to the global namespace.
-    //
-    // Fore more see https://github.com/humbug/php-scoper#whitelist
-    'whitelist' => [
-        'PHP_Typography\*',
-    ],
+    */
 
     // If `true` then the user defined constants belonging to the global namespace will not be prefixed.
     //
     // For more see https://github.com/humbug/php-scoper#constants--constants--functions-from-the-global-namespace
-    'whitelist-global-constants' => true,
+    'expose-global-constants' => true,
 
     // If `true` then the user defined classes belonging to the global namespace will not be prefixed.
     //
     // For more see https://github.com/humbug/php-scoper#constants--constants--functions-from-the-global-namespace
-    'whitelist-global-classes' => true,
+    'expose-global-classes' => true,
 
     // If `true` then the user defined functions belonging to the global namespace will not be prefixed.
     //
     // For more see https://github.com/humbug/php-scoper#constants--constants--functions-from-the-global-namespace
-    'whitelist-global-functions' => true,
+    'expose-global-functions' => true,
 ];
